1c561d1b7c8c1cb35ab0c17835caecb73cd22332
Wali Morris <48896622+walimorris@users.noreply.github.com>
2020-03-14 11:52:24 -0700

    Update OTPGEN.java

--- OTPGEN.java
+++ OTPGEN.java
@@ -4,7 +4,7 @@
  * The OTP GEN API allows a client program to easily generate an OTP for any 
  * user
  *
- * @author Wali Morris 
+ * @author Wali Morris<walimmorris@gmail.com>
  * 
  */
 
@@ -13,12 +13,18 @@ import java.util.*;
 public class OTPGEN { 
     private String otp; 
     private int length; 
-
+    
+    /**
+     * @param length : passed is the proposed length of the random password
+     */
     public OTPGEN(int length) { 
 	this.otp = null; 
         this.length = length; 
     }
-
+    
+    /**
+     * letters, number and special key lookup table for random generation
+     */
     final public void generatePassword() { 
 	String upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"; 
         String lower = "abcdefghijklmnopqrstuvwxyz"; 
@@ -26,27 +32,38 @@ public class OTPGEN {
         String special = "`~!@#$%^&*+=-/><";
 
 	String keys = upper + lower + number + special;  
-
+        
+	/**
+	 * Generate a new array that'll store each randomly generated character. Generates
+	 * a random character for the length of password. 
+	 */
 	Random r = new Random(); 
 	Character[] password = new Character[this.length]; 
 
 	for ( int i = 0; i < this.length; i++ ) {
             password[i] = keys.charAt(r.nextInt(keys.length()));
         }
-        try {
+        try { // Will attempt to throw null exception, generate an empty password String to begin adding chars
             if ( this.otp.equals(null) ) {
                 this.otp = "";
             }
         } catch ( NullPointerException e) {
             this.otp = "";
         }
+	/* Appends each character to OTP */ 
         Arrays.stream(password).forEach (key -> this.otp += key);
     }
-
+    
+    /** 
+     * @return password as a string, *add to string method here
+     */
     public String getPassword() {
         return this.otp;
     }
-
+    
+    /** 
+     * @return the length of the password
+     */ 
     public int getLength() {
         return this.length;
     }
